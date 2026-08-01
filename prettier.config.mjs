54edc267982373d7efd3f92297008832fd67dc20
Sergei Lutov <doublemakers@gmail.com>
2026-08-01 22:08:28 +0200

    chore: add eslint and prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+    arrowParens: "avoid",
+    htmlWhitespaceSensitivity: "ignore",
+    printWidth: 120,
+    semi: false,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "all",
+    useTabs: false,
+}
--- types/ts-reset.d.ts
+++ types/ts-reset.d.ts
@@ -2,7 +2,6 @@
 /* eslint-disable sonarjs/no-primitive-wrappers */
 /// <reference types="@total-typescript/ts-reset" />
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface BooleanConstructor {
     new (value?: unknown): Boolean
     <T>(value?: T): value is TSReset.NonFalsy<T>
