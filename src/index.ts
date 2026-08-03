12cee6108dd398900d58bfee239f5499bb8e0ce2
Sergei Lutov <doublemakers@gmail.com>
2026-08-03 14:31:04 +0200

    feat: export items

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from "./lib"
+export * from "./errors"
+export { initTranslations } from "./lib"
+export { Dictionary, Locale, LocaleStorage, Options, Translations } from "./types/lib"
+export { getLocaleLocalStorage } from "./utils"
