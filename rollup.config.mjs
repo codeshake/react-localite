cda6293d3419574f63aa4c9d6baf29c3bf4daf30
Sergei Lutov <doublemakers@gmail.com>
2026-08-03 14:53:57 +0200

    build: tsc alias

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,7 @@
-import { exec } from "node:child_process"
+import tscAlias from "rollup-plugin-tsc-alias"
 import typescript from "rollup-plugin-typescript2"
 import packageJson from "./package.json" with { type: "json" }
 import tsConfigJson from "./tsconfig.json" with { type: "json" }
-
-const tsAliasPlugin = () => ({
-    name: "tsAlias",
-    writeBundle: () => {
-        return new Promise((resolve, reject) => {
-            // eslint-disable-next-line sonarjs/no-os-command-from-path
-            exec("tsc-alias", (error, stdout, stderr) => {
-                if (stderr) {
-                    reject(new Error(stderr))
-                } else if (error) {
-                    reject(error)
-                } else {
-                    resolve(stdout)
-                }
-            })
-        })
-    },
-})
 
 const externalPackages = new Set([
     ...Object.keys(packageJson.dependencies ?? {}),
@@ -47,6 +29,6 @@ export default {
             exclude: ["node_modules"],
             useTsconfigDeclarationDir: true,
         }),
-        tsAliasPlugin(),
+        tscAlias(),
     ],
 }
